7216e3dd90f3ab71c2432901e92a8695e5248d78
Štěpán Cellar <stepan.cellar@gmail.com>
2019-07-02 19:17:27 +0200

    Configuration - app.properties app-mode development/production & constructor injection in ViewController

--- src/main/java/cz/cellar/springboot/ViewController.java
+++ src/main/java/cz/cellar/springboot/ViewController.java
@@ -1,5 +1,8 @@
 package cz.cellar.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,20 @@ import java.util.Date;
 
 @Controller
 public class ViewController {
+
+
+    private String appMode;
+//constructor injection for app-mode (from application.properties)
+    @Autowired
+    public ViewController(Environment environment){
+        appMode=environment.getProperty("app-mode");
+    }
+
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Štěpán Cellar");
-        model.addAttribute("mode", "development" );
+        model.addAttribute("mode", appMode );
         return "index";
     }
 }
